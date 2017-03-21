684a0cb0b3fcb229a99b1450fbc8eabc13b52cee
claes <claesca@outlook.com>
2017-03-21 16:12:52 +0100

    Added two tester that will show the number and its resulting fizz_buzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -23,3 +23,28 @@ def fizz_buzz(number)
   #  number
   #end
 end
+
+# Test the program visually with creating an array with numbers to test
+# Not the best solution I know. I could just put the puts in the 100.times loop
+# right away without any array, see below (i.e. 100.times {|n| puts "#{n+1} = #{fizz_buzz(n+1)}"}).
+# But I wanted to practice the array usage and looping over it
+def test_fizz_number(number_to_test_to)
+  output = []
+  number_to_test_to.times {|n| output << fizz_buzz(n+1)}
+  i=0 # Counter to compare with
+  output.each do |n|
+    i += 1
+    puts "#{i} = #{n}"
+  end
+end
+
+# Better test_fizz_number
+def test_fizz_number2(number_to_test_to)
+  number_to_test_to.times {|n| puts "#{n+1} = #{fizz_buzz(n+1)}"}
+end
+
+puts "test_fizz_number:"
+test_fizz_number(100)
+
+puts "test_fizz_number2:"
+test_fizz_number2(123)
